ac0e861cf4964e099e1da76fcf7e9c5d1f520f6f
Maxence Le Cheualier <maxence.le_cheualier@etu.minesparis.psl.eu>
2026-01-27 17:01:23 +0100

    Adding of docstrings in the fastareader class

--- src/finalproject/fasta_reader.py
+++ src/finalproject/fasta_reader.py
@@ -1,8 +1,26 @@
 class FastaReader:
-    def __init__(self, filepath):
+    """""
+    A class to read a fasta file and return a dictionary of sequences
+    """
+
+    def __init__(self, filepath : str) -> None:
+        """""
+        Initialize the FastaReader with the path to the fasta file
+
+        Args:
+            filepath (str) : The path to the fasta file 
+        """ 
         self.filepath = filepath
 
-    def read(self):
+    def read(self) -> dict(str, str):
+        """
+        Read the fasta file and return a dictionnary of sequences, distinguished by their names,and their variants if any.
+
+        Args :
+            None
+        Returns :
+            sequences (dict) : A dictionnary where keys are sequence names and values are listes of sequences (str)
+        """
 
         sequences = {}
         name = None 
