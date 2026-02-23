21065f7429886680d30228255352159be5ae0d00
Maxence Le Cheualier <maxence.le_cheualier@etu.minesparis.psl.eu>
2026-02-23 17:39:44 +0100

    Modification de la fonction Init

--- src/finalproject/needleman–wunsch.py
+++ src/finalproject/needleman–wunsch.py
@@ -3,15 +3,19 @@ class NeedlemanWunsch:
     Class to perform global sequence alignment using the Needleman-Wunsch algorithm.
     """
 
-    def __init__(self, match_score: int = 1, mismatch_penalty: int = -1, gap_penalty: int = -2):
+    def __init__(self, sequences : dict, match_score: int = 1, mismatch_penalty: int = -1, gap_penalty: int = -2):
         """
         Initialize the Needleman-Wunsch algorithm with scoring parameters.
 
         Args:
+            sequences (dict): The sequences to be aligned encoded using de fasta reader class.
             match_score (int): The score for a match (default: 1)
             mismatch_penalty (int): The penalty for a mismatch (default: -1)
             gap_penalty (int): The penalty for a gap (default: -2)
         """
+        self.sequences = sequences
         self.match_score = match_score
         self.mismatch_penalty = mismatch_penalty
         self.gap_penalty = gap_penalty
+    
+    def 
