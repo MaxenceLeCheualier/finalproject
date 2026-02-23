9feaff2d7bc8be074ed83f80f9a13ea427109858
Maxence Le Cheualier <maxence.le_cheualier@etu.minesparis.psl.eu>
2026-02-23 17:47:26 +0100

    Début de la fonction construisant la matrice des scores

--- src/finalproject/needleman–wunsch.py
+++ src/finalproject/needleman–wunsch.py
@@ -18,4 +18,25 @@ class NeedlemanWunsch:
         self.mismatch_penalty = mismatch_penalty
         self.gap_penalty = gap_penalty
     
-    def 
+    def score_matrix(self, seq : str, var : str) :
+        """
+        Build the score matrix for the given sequences, using the Needleman-Wunsch algorithm.
+        Args:
+            seq (str): The first sequence to be aligned.
+            var (str): The second sequence to be aligned.
+        """
+
+        long_seq = len(seq) + 1
+        long_var = len(var) + 1
+
+        matrix = [[0 for i in range(long_seq)] for _ in range(long_var)]
+
+        # Initialize the first row and column of the score matrix
+        for i in range(long_seq):
+            matrix[0][i] = self.gap_penalty * i
+        for j in range(long_var):
+            matrix[j][0] = self.gap_penalty * j
+        
+        
+    
+
