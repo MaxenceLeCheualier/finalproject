cdf560d0c8442be9b1036a99515829103cdb6b93
Maxence Le Cheualier <maxence.le_cheualier@etu.minesparis.psl.eu>
2026-03-06 10:48:18 +0100

    Verifiction de la fonction main : quand je rentre uv run finalproject -i src/finalproject/seq_to_align.fasta dans le terminal, ca marche wq

--- src/finalproject/main.py
+++ src/finalproject/main.py
@@ -1,16 +1,16 @@
 import argparse 
 import sys 
-from fasta_reader import FastaReader
-from needleman_wunsch import NeedlemanWunsch 
+from finalproject.fasta_reader import FastaReader
+from finalproject.needleman_wunsch import NeedlemanWunsch 
 
 def main():
     parser = argparse.ArgumentParser(description= "Sequence alignment using Needleman-Wunsch algorithm")
     
     parser.add_argument("-i", "--input", required = True, help = "Fasta file input")
     parser.add_argument("-o", "--output", default = "-", help = "Output file")
-    parser.add_argument("-m", "--match-score", type = int, default = "-", help = "Score for a match")
+    parser.add_argument("-m", "--match-score", type = int, default = "1", help = "Score for a match")
     parser.add_argument("-x", "--mismatch-score", type = int, default = -1, help = "Score for a mismatch")
-    parser.add_argument("-j", "--indel_score", type = int, default = -2, help = "Score for instertion / deletion")
+    parser.add_argument("-j", "--indel-score", type = int, default = -2, help = "Score for instertion / deletion")
     parser.add_argument("-v", "--verbose", action = "count", default = 0, help = "Verbosity level") 
 
     args = parser.parse_args()
