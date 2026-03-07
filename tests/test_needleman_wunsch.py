e64720d468ce9480252e56c3ab9f07d277f28d73
Maxence Le Cheualier <maxence.le_cheualier@etu.minesparis.psl.eu>
2026-03-07 11:47:01 +0100

    Test avec un mismatch et avec un gap

--- tests/test_needleman_wunsch.py
+++ tests/test_needleman_wunsch.py
@@ -9,7 +9,35 @@ def test_needleman_simple_match():
     }
 
     nw = NeedlemanWunsch(sequences)
-    aligned = nw.align()
+    aligned = nw.align(sequences)
 
     assert aligned["seq1"][0] == "AAAA"
     assert aligned["seq1"][1] == "AAAA"
+
+def test_needleman_one_mismatch():
+
+    sequences = {
+        "seq1": ["AAAA", "TAAA"]
+    }
+
+    nw = NeedlemanWunsch(sequences)
+    aligned = nw.align(sequences)
+
+    seq, var = aligned["seq1"]
+
+    assert len(seq) == len(var)
+    assert seq == "AAAA"
+    assert var == "TAAA"
+
+def test_needleman_one_gape():
+
+    sequences = { "seq1" : ["ATGCTGAT", "ATGCTGT"]}
+
+    nw = NeedlemanWunsch(sequences)
+    aligned = nw.align(sequences)
+
+    seq, var = aligned["seq1"]
+
+    assert seq == "ATGCTGAT"
+    assert var == "ATGCTG-T"
+
